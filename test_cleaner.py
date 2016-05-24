1044c9c24d3fc8acb8b5609c4500074c35fc5ed4
Helge Stenström <h.stenstrom@gmail.com>
2016-05-24 08:10:33 +0200

    KOmmentarer

--- test_cleaner.py
+++ test_cleaner.py
@@ -31,5 +31,37 @@ class RunDirTests(unittest.TestCase):
         'A results directory has a few results files, but no sub directory.'
         self.assertFalse(c.hasFilesOnly('/'))
 
+# I slutändan vill jag att mitt program ska rensa bland filerna. Vägen
+# dit kan gå via att ändra namn på mappar som ska rensas bort.
+# Jag vill komma dit genom att skapa funktioner som kan testas
+# oberoende av något filsystem.
+
+# Saker att filtrera på:
+# - att mappen har rätt nivåer.
+# - Att nödvändiga filer finns där, annars är mappen värdelös.
+
+# Rätt nivåer är:
+# 1. Roten, den som anges i OneTE GUI
+# 2. Nivå med mappar som namnges enligt TOL
+# 3. Nivå med mappar som har namn med tidsstämpel
+# 4. Nivå med filer, men inga undermappar.
+
+# Användbara funktioner
+# glob.glob(root + '/*/*/*')
+# returnerar, tror jag, filerna, och ska inte returnera några mappar,
+# om det är rätt typ av resultatmapp.
+# Se
+# http://stackoverflow.com/questions/7159607/list-directories-with-a-specified-depth-in-python
+
+# Problemet just nu, är att jag inte kan komma på någon användbar
+# funktion att skriva, som inte antingen är beroende av befintliga
+# filer (vilket kan simuleras, men det är jobbigt), eller har
+# sideffekter (vilka är önskvärda, men inte för testning).
+
+# re.search('([0-9]+_[0-9]+_[0-9]+-LPA_109_332_1-[0-9]{1,3})', '2_152_82-LPA_109_332_1-906').group(0)
+# d = '2_152_82-LPA_109_332_1-906'
+# re.search('(\d{1,3}_\d{1,3}-LPA_109_332_1-\d{1,3})', d).group(0)
+# re.search('(\d_152_82-LPA_109_332_1-\d{1,3})', d).group(0)
+
 if __name__ == '__main__':
     unittest.main()
